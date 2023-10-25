995a7a5b556f66f8a0364ef6852e92546e40b6cd
MathiasDziurkowski <128103391+MathiasDziurkowski@users.noreply.github.com>
2023-10-24 21:22:18 -0300

    moving between screens

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/HomeScreen';
+import ListScreen from './src/ListScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="List" component={ListScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
---
+++ src/ListScreen/ListScreen.js
@@ -0,0 +1,12 @@
+import { View, Text, Button} from 'react-native';
+
+;
+
+function ListScreen({navigation}) {
+    return(
+        <View>
+            <Text>List Screen</Text>
+        </View>
+    )
+}
+export default ListScreen
