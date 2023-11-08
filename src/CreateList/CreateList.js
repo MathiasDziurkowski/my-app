fe44f1ff1d8f4d4855e3dc5a5ab6bfa10c95f2ba
MathiasDziurkowski <128103391+MathiasDziurkowski@users.noreply.github.com>
2023-11-07 21:49:33 -0300

    deu meio certo

--- src/CreateList/CreateList.js
+++ src/CreateList/CreateList.js
@@ -1,19 +1,40 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { styled } from "nativewind";
 import { useState } from "react";
 import { Button, Pressable, Text, TextInput, View } from "react-native";
+import metadata from '../../storage.metadata.json'
 
 const StyledView = styled(View)
 
 
 function CreateList({ navigation }) {
     const [name, setName] = useState("");
-    
+
+    const getList = async () => {
+        const listName = await AsyncStorage.getItem(metadata.LIST.LISTNAME);
+        if (listName) {
+            return JSON.parse(listName) 
+        }
+    }
+
+    const saveList = async () => {
+        var saveList = await getList() || "";
+        saveList.push(name) 
+        await AsyncStorage.setItem(metadata.LIST.LISTNAME, JSON.stringify(saveList))
+    }
+
+    const handleClick = async () => {
+        await saveList()
+        navigation.navigate('List')
+    }
+
+
 
     return (
         <StyledView className="flex-1 flex-col gap-6 justify-center items-center dark: bg-gray-900">
             <Text className="dark: text-white">Create List</Text>
             <TextInput className="dark: text-white border-2 border-white rounded-xl" placeholder="Coloque o nome da sua lista" onChangeText={setName}></TextInput>
-            <Pressable className="dark: p-6 bg-black rounded-xl" onPress={() => { navigation.navigate('List', {name: name}) }}><Text className="text-white">Create your List</Text></Pressable>
+            <Pressable className="dark: p-6 bg-black rounded-xl" onPress={handleClick}><Text className="text-white">Create your List</Text></Pressable>
         </StyledView>
     )
 }
--- src/ListScreen/ListScreen.js
+++ src/ListScreen/ListScreen.js
@@ -1,36 +1,59 @@
 import { useIsFocused } from '@react-navigation/native';
 import { useEffect, useMemo, useState } from 'react';
-import { View, Text, Button, Pressable} from 'react-native';
+import { View, Text, Button, Pressable } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Icon } from 'react-native-elements'
-function ListScreen({navigation , route}) {
-    const {name} = route.params
-    const [lista, setListas] = useState([name])
+import metadata from '../../storage.metadata.json'
+import { list } from '@material-tailwind/react';
+function ListScreen({ navigation }) {
+    const [lista, setListas] = useState([])
     const focus = useIsFocused();
-    
+
+    useEffect(() => { getList() }, [focus])
+
+    const saveList = async () => {
+        const saveList = lista || "";
+        await AsyncStorage.setItem(metadata.LIST.LISTNAME, JSON.stringify(saveList))
+    }
+    const getList = async () => {
+        const listName = await AsyncStorage.getItem(metadata.LIST.LISTNAME);
+        if (listName) {
+            setListas(JSON.parse(listName))
+        }
+    }
+
+
     const deleteClick = () => {
         const newLista = [lista]
-        newLista.splice(0,1)
+        newLista.splice(0, 1)
         setListas(newLista)
     }
 
-    
-    const listDisplay = useMemo(() => { 
-        
-        if (lista.length > 0){
-        return(
-            <View className="flex flex-row  justify-self-center gap-5 border rounded-xl dark: border-white  bg-emerald-600">
-                <Text className="dark: text-white">{lista}</Text>
-                <Pressable onPress={deleteClick}><Icon name='delete'></Icon></Pressable>
-                <Pressable><Icon name='edit'></Icon></Pressable>
+
+    const listDisplay = useMemo(() => {
+
+        return (
+            <View className="flex-1 gap-5">
+                {
+                    lista.map((item, index) => {
+                        return (
+
+                            <View className="flex flex-row  justify-self-center gap-5 border rounded-xl dark: border-white  bg-emerald-600">
+                                <Text className="dark: text-white">{item}</Text>
+                                <Pressable onPress={deleteClick}><Icon name='delete'></Icon></Pressable>
+                                <Pressable><Icon name='edit'></Icon></Pressable>
+                            </View>
+                        )
+                    })
+                }
             </View>
         )
-        }
     }, [lista])
-    return(
+    return (
         <View className="flex-auto flex-col gap-5 justify-center items-center align-middle dark: bg-gray-900 ">
             {listDisplay}
             <Text className="dark: text-white">List Screen</Text>
-            <Pressable className="dark: bg-black p-6"  onPress={() => {navigation.navigate('CreateList')}}><Text className="dark: text-white">Go to List Screen</Text></Pressable>
+            <Pressable className="dark: bg-black p-6" onPress={() => { navigation.navigate('CreateList') }}><Text className="dark: text-white">Go to List Screen</Text></Pressable>
         </View>
     )
 }
